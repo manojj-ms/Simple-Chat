b37d2a0a80d5ac2575484bf61a5a71a97f72634e
Manoj <manojkumarmsd9204@gmail.com>
2021-07-23 19:39:27 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true,
+        allowEIO4: true
+    },
+    transport: ['websocket']
+});
+
+io.on('connection', socket => {
+    socket.on('message', ({ name, message }) => {
+        io.emit('message', { name, message })
+    })
+})
+
+http.listen(4000, function () {
+    console.log('listening on port 4000')
+})
